8fd4b9ac0df86a3da1ffd3b662275bd570c25b4e
simon <sajmoooonsk@gmail.com>
2022-09-27 09:16:59 +0200

    feat: add builder pattern

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -3,7 +3,8 @@ package builder;
 public class Main {
 
     public static void main(String[] args){
-
+        Pizza pizza = Pizza.createPizza().setBacon(true).setCheese(true).setSize("Small").build();
+        System.out.println(pizza.toString());
     }
 
 }
